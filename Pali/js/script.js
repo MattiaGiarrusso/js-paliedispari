1b97076314935e285455c264d4616f8c5f5e58aa
Mattia Giarrusso <giarrusso.m@yahoo.com>
2024-03-14 17:26:19 +0100

    Feat: add prompt to insert the word

--- Pali/js/script.js
+++ Pali/js/script.js
@@ -1,3 +1,7 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 // Su internet ci sono soluzioni con join e reverse, non potete usarle!
+
+// Chiedo all’utente di inserire una parola
+const userWord = prompt('Inserisci una parola');
+
